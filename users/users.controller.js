aaeb5364096c85348c47b961fec1fa16a0e42e75
Martin Kraus Larsen <26963547+mklarsen@users.noreply.github.com>
2022-08-27 19:02:23 +0200

    added manager

--- users/users.controller.js
+++ users/users.controller.js
@@ -54,7 +54,7 @@ function createSchema(req, res, next) {
         title: Joi.string().required(),
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
-        role: Joi.string().valid(Role.Admin, Role.User).required(),
+        role: Joi.string().valid(Role.Admin, Role.Manager, Role.User).required(),
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
         confirmPassword: Joi.string().valid(Joi.ref('password')).required()
@@ -67,7 +67,7 @@ function updateSchema(req, res, next) {
         title: Joi.string().empty(''),
         firstName: Joi.string().empty(''),
         lastName: Joi.string().empty(''),
-        role: Joi.string().valid(Role.Admin, Role.User).empty(''),
+        role: Joi.string().valid(Role.Admin, Role.Manager, Role.User).empty(''),
         email: Joi.string().email().empty(''),
         password: Joi.string().min(6).empty(''),
         confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
